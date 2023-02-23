32ff3acfd160888aa58b0899e7e65020d40bed61
thelight0804 <thelight0804@gmail.com>
2023-02-23 16:32:53 +0900

    Add react project
    - README.md
        - Project에 관한 간단한 내용 추가
    - react 프로젝트 생성
    - MUI (Material-UI) CSS 프레임워크 사용

---
+++ utau/src/App.js
@@ -0,0 +1,14 @@
+import {React} from 'react';
+import './App.css';
+
+import Title from './page/Title.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Title/>
+    </div>
+  );
+}
+
+export default App;
---
+++ utau/src/page/Title.js
@@ -0,0 +1,7 @@
+function Title(){
+  return(
+    <h1>Utau</h1>
+  )
+}
+
+export default Title;
