1aeac6bc9870e1bfb5177986c86da7a58c7a0c1e
thelight0804 <thelight0804@gmail.com>
2023-02-24 14:02:03 +0900

    Add title, search text field, tabs
    **App.js**
    
    - **createTheme** : 적용할 색깔 탬플릿
        - primary : teal[500] //#009688
        - secondary : teal[600] //#00897b
        - info : teal[700] // #00796b
    - **<ThemeProvider theme={theme}>** : 색깔 탬플릿 적용
    - <**TextField**> : 검색 필드
        - **InputProps={{endAdornment}**} : 검색 돋보기 아이콘
    - **<Tabs>** : 인기 차트, 최신 곡 선택 탭
    
    **Titlle.js** : 컴포넌트로 바꿀 필요성이 사라저 삭제하였습니다

--- utau/src/App.js
+++ utau/src/App.js
@@ -1,12 +1,65 @@
-import {React} from 'react';
+import {React, useState} from 'react';
 import './App.css';
+import {TextField, createTheme, ThemeProvider, Tabs, Tab} from '@mui/material';
+import { teal } from '@mui/material/colors';
+import SearchIcon from '@mui/icons-material/Search';
 
-import Title from './page/Title.js';
+
+//theme color
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: teal[500],
+      // #009688
+    },
+    secondary: {
+      main: teal[600],
+      // #00897b
+    },
+    info:{
+      main: teal[700]
+      // #00796b
+    },
+  },
+});
 
 function App() {
+  const [Tabvalue, setTabvalue] = useState(0);
+
+  const handleChange = (event, newTabvalue) => {
+    setTabvalue(newTabvalue);
+  };
+
   return (
     <div className="App">
-      <Title/>
+      <ThemeProvider theme={theme}>
+        <h1>Utau</h1>
+
+        {/* input search */}
+        <TextField
+          fullWidth
+          id="standard-basic"
+          label="제목, 가수, 작품, 가사로 검색"
+          variant="standard"
+          InputProps={{
+            endAdornment: <SearchIcon fontSize="large" color="primary" />,
+          }}
+        />
+
+        {/* tab */}
+        <Tabs
+          value={Tabvalue}
+          onChange={handleChange}
+          textColor="secondary"
+          indicatorColor="secondary"
+          variant='fullWidth'
+          style={{margin: 10 + 'px'}}
+          // aria-label="secondary tabs example"
+        >
+          <Tab value={0} label="인기 차트" />
+          <Tab value={1} label="최신 곡" />
+        </Tabs>
+      </ThemeProvider>
     </div>
   );
 }
--- utau/src/page/Title.js
+++
@@ -1,7 +0,0 @@
-function Title(){
-  return(
-    <h1>Utau</h1>
-  )
-}
-
-export default Title;
